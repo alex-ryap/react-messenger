38698e923cd9fd2cf59c85ba50e37cfe695a6e7e
Alexandr <alexryapolov36@gmail.com>
2021-11-01 20:55:17 +0400

    add new logic chat

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
 import { Message } from './components/Message/Message';
-import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { InputMessage } from './components/InputMessage/InputMessage';
+import './App.scss';
 
 function App() {
-  const [text, setText] = useState('Some message');
+  const [messageId, setMessageId] = useState(1);
+  const [messageList, setMessageList] = useState([]);
+
+  // Need use callback with prevState
+  const addNewMessage = (text, author = 'Me') => {
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    };
+    const message = {
+      id: messageId,
+      author: author,
+      text: text,
+      date: new Intl.DateTimeFormat('ru-RU', options).format(new Date()),
+    };
+
+    setMessageList([...messageList, message]);
+    setMessageId(messageId + 1);
+  };
+
+  // Line 34:6:  React Hook useEffect has a missing dependency: 'addNewMessage'. Either include it or remove the dependency array
+  useEffect(() => {
+    if (messageList.length >= 1) {
+      if (messageList[messageList.length - 1].author === 'Me') {
+        setTimeout(() => addNewMessage('Hello, i am Robot', 'Robot'), 2000);
+      }
+    }
+  }, [messageList]);
 
   return (
     <div className="App">
-      <Message message={text} />
-      <InputMessage change={setText} />
+      <div className="container-fluid">
+        <div className="chat">
+          <div className="chat__header">
+            <h3>
+              Chat with <span>Robot</span>
+            </h3>
+          </div>
+          <div className="chat__history">
+            <div className="messages">
+              {messageList.map((message) => {
+                return <Message message={message} key={message.id} />;
+              })}
+            </div>
+          </div>
+          <div className="chat__form">
+            <InputMessage submit={addNewMessage} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/InputMessage/InputMessage.jsx
+++ src/components/InputMessage/InputMessage.jsx
@@ -1,16 +1,35 @@
+import { useState } from 'react';
 import './InputMessage.scss';
 
-export const InputMessage = ({ change }) => {
-  function handleChange(event) {
-    change(event.target.value);
-  }
+export const InputMessage = ({ submit }) => {
+  const [message, setMessage] = useState('');
+
+  const handlerSubmit = (event) => {
+    event.preventDefault();
+    if (message) {
+      submit(message);
+      setMessage('');
+    }
+  };
+
+  const handlerChange = (event) => {
+    setMessage(event.target.value);
+  };
 
   return (
-    <form className="form">
-      <label className="form__label" htmlFor="">
-        Type your message here:
-      </label>
-      <input className="form__input" type="text" onChange={handleChange} />
+    <form className="form" onSubmit={handlerSubmit}>
+      <label className="form__label">Type your message here:</label>
+      <div className="form__actions">
+        <input
+          className="form__input"
+          type="text"
+          onChange={handlerChange}
+          value={message}
+        />
+        <button className="form__btn" type="submit">
+          Send
+        </button>
+      </div>
     </form>
   );
 };
