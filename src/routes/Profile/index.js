88a424cd47c2c29a070ab94e629f9f0f8b392dd9
Alexandr <alexryapolov36@gmail.com>
2021-11-23 11:25:37 +0400

    add action, reducer and checkbox in profile

--- src/routes/Profile/index.js
+++ src/routes/Profile/index.js
@@ -1,12 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { UserProfile } from '../../components/UserProfile';
+import { toggleTheme } from '../../store/Profile/actions';
+import { selectThemeCheckbox } from '../../store/Profile/selectors';
 import './style.scss';
 
 export const Profile = () => {
+  const themeCheckbox = useSelector(selectThemeCheckbox);
+  const dispatch = useDispatch();
+
+  const handleChecked = () => {
+    dispatch(toggleTheme);
+  };
+
   return (
     <div className="profile">
       <div className="profile__content">
         <h1>Profile</h1>
         <UserProfile />
+        <label className="profile__theme">
+          <input
+            className="profile__checkbox"
+            type="checkbox"
+            value={themeCheckbox}
+            onChange={handleChecked}
+          />
+          Dark theme
+        </label>
       </div>
     </div>
   );
---
+++ src/store/Profile/actions.js
@@ -0,0 +1,5 @@
+export const TOGGLE_THEME = 'PROFILE::TOGGLE_THEME';
+
+export const toggleTheme = {
+  type: TOGGLE_THEME,
+};
